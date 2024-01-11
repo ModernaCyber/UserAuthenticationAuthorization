f76b70e1a286d35b2b1b7d7b0e283db6ede9f90f
jhn kash <87578882+ModernaCyber@users.noreply.github.com>
2024-01-11 13:18:14 +0300

    k

--- src/middleware/authJWT.ts
+++ src/middleware/authJWT.ts
@@ -12,51 +12,52 @@ export const authJWTByRole = (roles: string[]) => async (
   ): Promise<void>=>{
 
     const tokenSecret = process.env.TOKEN_SECRET || ACCESS_TOKEN_SECRET
-    const token = req.headers.authorization?.split(" ")[1] || req.cookies.token;
-      if (!token) {
-        res.status(401).json({
-          status: 401,
-          success: false,
-          message: "Access token is required",
-          error: "",
-        });
-        return;
-      }
-
-      const decodedData:any = await jwt.verify(token, tokenSecret) ;
-
-
-      const { UserInfo } = decodedData
-
-      const { id , role } = UserInfo
-
-      const user:any = await User.findByPk(id);
-
-      if (!user) {
-        res.status(401).json({
-          status: 401,
-          success: false,
-          message: "User not found",
-          error: "",
-        });
-        return;
-      }
-
-      // check if the user is authorised to the role
-      if (!roles.includes(user.role)) {
-        res.status(403).json({
-          status: 403,
-          success: false,
-          message: "Insufficient permissions",
-          error: "",
-        });
-        return;
-      }
-      
-      next();
+  
 
     try {
-        
+          
+    const token = req.headers?.authorization?.split(" ")[1] || req?.cookies?.token;
+    if (!token) {
+      res.status(401).json({
+        status: 401,
+        success: false,
+        message: "Access token is required",
+        error: "",
+      });
+      return;
+    }
+
+    const decodedData:any = await jwt.verify(token, tokenSecret) ;
+
+
+    const { UserInfo } = decodedData
+
+    const { id , role } = UserInfo
+
+    const user:any = await User.findByPk(id);
+
+    if (!user) {
+      res.status(401).json({
+        status: 401,
+        success: false,
+        message: "User not found",
+        error: "",
+      });
+      return;
+    }
+
+    // check if the user is authorised to the role
+    if (!roles.includes(user.role)) {
+      res.status(403).json({
+        status: 403,
+        success: false,
+        message: "Insufficient permissions",
+        error: "",
+      });
+      return;
+    }
+    
+    next();
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Internal Server Error' });
